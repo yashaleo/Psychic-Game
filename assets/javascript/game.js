0be1b893cea8b332fea6d31a0da9780f7e95a855
yashaleo <yashaleo@me.com>
2019-06-10 01:14:02 -0500

    working version, with only add console.log glitch

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -36,62 +36,55 @@ var letters = [
 var playerGuessList = [];
 var playerGuesses = document.getElementById("playerGuesses").innerHTML;
 
+var compPick = function() {
+  compChoice = letters[Math.floor(Math.random() * letters.length)];
+  console.log("Comp Picked " + compChoice);
+};
+
 var gameStart = function() {
   playerGuessesLeft = 9;
+  compPick();
   document.getElementById("playerWins").innerHTML = "Wins: " + playerWins;
   document.getElementById("playerLosses").innerHTML = "Losses: " + playerLosses;
   document.getElementById("playerGuessesLeft").innerHTML =
     "Guesses Left: " + playerGuessesLeft;
   console.log("Start OVER PLEASE!");
-  compPick();
-  playerGuessList.empty();
-  playerGuesses = "You guessed: " + playerGuessList;
+  playerGuesses = [];
 };
-
-var compPick = function() {
-  compChoice = letters[Math.floor(Math.random() * letters.length)];
-  console.log("Comp Picked! " + compChoice);
-};
-
-var letterChecker = function() {
-    if (playerGuesses === compChoice) {
-      alert("Got it!");
-      playerWins++;
-      compPick();
-      playerGuessList;
-      gameStart();
-    //   document.getElementById("playerGuesses").innerHTML =
-    //     "Guessed: " + playerGuessList;
-    } else {
-      playerGuessesLeft--;
-      document.getElementById("playerGuessesLeft").innerHTML =
-        "Guesses Left: " + playerGuessesLeft;
-    }
-    if (playerGuessesLeft == 0) {
-      alert("You lost!");
-      playerLosses++;
-      document.getElementById(playerLosses).innerHTML = "Losses: " + playerLosses;
-      playerGuessList = [];
-      gameStart();
-    }
-  };
 
 document.onkeyup = function(event) {
   playerGuesses = String.fromCharCode(event.keyCode).toLocaleLowerCase();
-  console.log("player picked!: " + playerGuesses);
+  console.log("player picked " + playerGuesses);
   letterChecker();
   playerGuessList.push(playerGuesses);
-  console.log("playerGuessList");
   document.getElementById("playerGuesses").innerHTML =
-    "You guessed: " + playerGuessList;
+    "Your Guesses So Far: " + playerGuessList;
 };
-
-
 
 document.getElementById("playerWins").innerHTML = "Wins: " + playerWins;
 document.getElementById("playerLosses").innerHTML = "Losses: " + playerLosses;
 document.getElementById("playerGuessesLeft").innerHTML =
   "Guesses Left: " + playerGuessesLeft;
 
-// computer picks new letter
-compPick();
+var letterChecker = function() {
+  if (playerGuesses == compChoice) {
+    alert("Got it!");
+    playerWins++;
+    document.getElementById("playerGuesses").innerHTML =
+      "Your Guesses So Far: " + playerGuessList;
+    playerGuessList = [];
+    gameStart();
+  }
+  if (playerGuessesLeft == 0) {
+    alert("You lost!");
+    playerLosses++;
+    document.getElementById("playerLosses").innerHTML =
+      "Losses: " + playerLosses;
+    playerGuessList = [];
+    gameStart();
+  } else {
+    playerGuessesLeft--;
+    document.getElementById("playerGuessesLeft").innerHTML =
+      "Guesses Left: " + playerGuessesLeft;
+  }
+};
