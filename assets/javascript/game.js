0547d4a5bf3b44d5da74494bcf88151111f261a3
yashaleo <yashaleo@me.com>
2019-06-09 23:06:55 -0500

    added document and game functions, figuring out computer choices compared to player choices

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,6 +2,7 @@ var playerWins = 0;
 var playerLosses = 0;
 var playerGuessesLeft = 0;
 var playerGuesses = 0;
+var compPick = 0;
 
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
@@ -14,4 +15,28 @@ document.getElementById("playerGuessesLeft").innerHTML = "Guesses Left: " + play
 document.getElementById("playerGuesses").innerHTML = "Your Guesses So Far: " + playerGuesses;
 }
 
+var compPick = function() {
+compChoice = letters[Math.floor(Math.random() * letters.length)];
+}
 
+document.onkeyup = function (event) {
+    playerGuesses = String.fromCharCode(event.keyCode)
+    .toLocaleLowerCase();
+    letterChecker();
+    playerGuessList.push(playerGuesses);
+    document.getElementById("playerGuesses").innerHTML = "You guessed:" + playerGuessList;
+}
+
+var letterChecker = function () {
+if (playerGuesses === compChoice) {
+    alert ("Got it!");
+    playerWins++;
+    compPick();
+    gameStart();
+    document.getElementById("playerGuesses").innerHTML = "Guessed: " + playerGuessList;
+}
+else {
+
+}
+
+}
